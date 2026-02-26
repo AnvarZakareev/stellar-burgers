a15d5ba01300f30c7226aaded79d30d8a8d5b0cf
AnvarZakareev <zanvarr@mail.ru>
2026-02-26 12:31:21 +0800

    add modal

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,11 @@
-import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom';
+import {
+  Routes,
+  Route,
+  BrowserRouter,
+  Navigate,
+  useNavigate,
+  useLocation
+} from 'react-router-dom';
 import {
   ConstructorPage,
   Feed,
@@ -12,8 +19,13 @@ import {
 } from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
-
-import { AppHeader, ProtectedRoute } from '@components';
+import {
+  AppHeader,
+  ProtectedRoute,
+  Modal,
+  OrderInfo,
+  IngredientDetails
+} from '@components';
 import { Preloader } from '@ui';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from '../../services/store/store';
@@ -21,6 +33,9 @@ import { fetchIngredients } from '../../services/ingredients/actions';
 
 const App = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const background = location.state?.background;
 
   const { ingredients, isLoading, error } = useSelector((state) => ({
     ingredients: state.ingredients.ingredients,
@@ -32,6 +47,10 @@ const App = () => {
     dispatch(fetchIngredients());
   }, [dispatch]);
 
+  const handleCloseModal = () => {
+    navigate(-1);
+  };
+
   return (
     <div className={styles.app}>
       <AppHeader />
@@ -42,59 +61,103 @@ const App = () => {
           {error}
         </div>
       ) : ingredients.length > 0 ? (
-        <Routes>
-          <Route path='/' element={<ConstructorPage />} />
-          <Route path='/feed' element={<Feed />} />
-          <Route
-            path='/login'
-            element={
-              <ProtectedRoute onlyUnAuth>
-                <Login />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path='/register'
-            element={
-              <ProtectedRoute onlyUnAuth>
-                <Register />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path='/forgot-password'
-            element={
-              <ProtectedRoute onlyUnAuth>
-                <ForgotPassword />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path='/reset-password'
-            element={
-              <ProtectedRoute onlyUnAuth>
-                <ResetPassword />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path='/profile'
-            element={
-              <ProtectedRoute>
-                <Profile />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path='/profile/orders'
-            element={
-              <ProtectedRoute>
-                <ProfileOrders />
-              </ProtectedRoute>
-            }
-          />
-          <Route path='*' element={<NotFound404 />} />
-        </Routes>
+        <>
+          <Routes location={background || location}>
+            <Route path='/' element={<ConstructorPage />} />
+            <Route path='/feed' element={<Feed />} />
+            <Route path='/feed/:number' element={<OrderInfo />} />
+            <Route path='/ingredients/:id' element={<IngredientDetails />} />
+            <Route
+              path='/login'
+              element={
+                <ProtectedRoute onlyUnAuth>
+                  <Login />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path='/register'
+              element={
+                <ProtectedRoute onlyUnAuth>
+                  <Register />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path='/forgot-password'
+              element={
+                <ProtectedRoute onlyUnAuth>
+                  <ForgotPassword />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path='/reset-password'
+              element={
+                <ProtectedRoute onlyUnAuth>
+                  <ResetPassword />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path='/profile'
+              element={
+                <ProtectedRoute>
+                  <Profile />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path='/profile/orders'
+              element={
+                <ProtectedRoute>
+                  <ProfileOrders />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path='/profile/orders/:number'
+              element={
+                <ProtectedRoute>
+                  <OrderInfo />
+                </ProtectedRoute>
+              }
+            />
+            <Route path='*' element={<NotFound404 />} />
+          </Routes>
+
+          {/* Модальные окна */}
+          {background && (
+            <Routes>
+              <Route
+                path='/feed/:number'
+                element={
+                  <Modal title='' onClose={handleCloseModal}>
+                    <OrderInfo />
+                  </Modal>
+                }
+              />
+              <Route
+                path='/ingredients/:id'
+                element={
+                  <Modal title='Детали ингредиента' onClose={handleCloseModal}>
+                    <IngredientDetails />
+                  </Modal>
+                }
+              />
+              <Route
+                path='/profile/orders/:number'
+                element={
+                  <ProtectedRoute>
+                    <Modal title='' onClose={handleCloseModal}>
+                      <OrderInfo />
+                    </Modal>
+                  </ProtectedRoute>
+                }
+              />
+            </Routes>
+          )}
+        </>
       ) : (
         <div className={`${styles.title} text text_type_main-medium pt-4`}>
           Нет игредиентов
