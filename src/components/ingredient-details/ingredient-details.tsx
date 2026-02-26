c886682c31c41b7e785b0af69389bbe5caf96bbc
AnvarZakareev <zanvarr@mail.ru>
2026-02-26 17:05:16 +0800

    Modall IngredientDetails work

--- src/components/ingredient-details/ingredient-details.tsx
+++ src/components/ingredient-details/ingredient-details.tsx
@@ -1,11 +1,17 @@
 import { FC } from 'react';
 import { Preloader } from '../ui/preloader';
 import { IngredientDetailsUI } from '../ui/ingredient-details';
+import { useSelector } from '../../services/store/store';
+import { useParams } from 'react-router-dom';
+import type { RootState } from '../../services/store/store';
 
 export const IngredientDetails: FC = () => {
-  /** TODO: взять переменную из стора */
-  const ingredientData = null;
+  const { id } = useParams<{ id: string }>();
 
+  const ingredientData = useSelector((state: RootState) =>
+    state.ingredients.ingredients.find((ingredient) => ingredient._id === id)
+  );
+  console.log(ingredientData);
   if (!ingredientData) {
     return <Preloader />;
   }
