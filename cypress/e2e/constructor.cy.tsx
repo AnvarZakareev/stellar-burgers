21f7c0339b3df02b92e48707591b82f49b2a3662
AnvarZakareev <zanvarr@mail.ru>
2026-03-02 17:27:44 +0800

    test constructor OK

--- cypress/e2e/constructor.cy.tsx
+++ cypress/e2e/constructor.cy.tsx
@@ -1,18 +1,22 @@
 describe('Burger Constructor', () => {
   beforeEach(() => {
-    // Перехватываем запрос на ингридиенты и отдаём наш мок
     cy.intercept('GET', '**/api/ingredients', {
       fixture: 'ingredients.json'
     }).as('getIngredients');
-    // Открываем главную страницу — адаптируй под свой роут, если нужно
     cy.visit('/');
-    // Ждём загрузки наших моковых ингредиентов
     cy.wait('@getIngredients');
   });
 
-  it('Показывает список ингредиентов из моков', () => {
-    // Например, ищем название одного из ингредиентов из фикстуры
-    cy.contains('Краторная булка N-200i');
-    cy.contains('Мясо бессмертных моллюсков Protostomia');
+  it('Ддобавление ингредиента из списка в конструктор', () => {
+    cy.contains('Краторная булка N-200i').parent().contains('Добавить').click();
+    cy.contains('Мясо бессмертных моллюсков Protostomia')
+      .parent()
+      .contains('Добавить')
+      .click();
+
+    cy.get('[class*=constructor]').contains('Краторная булка N-200i');
+    cy.get('[class*=constructor]').contains(
+      'Мясо бессмертных моллюсков Protostomia'
+    );
   });
 });
