c9aae48f39143039a3d52659811ed0d55e221c7d
AnvarZakareev <zanvarr@mail.ru>
2026-03-03 02:01:03 +0800

    jest constructorSlice

--- __tests__/constructorSlice.test.ts
+++ __tests__/constructorSlice.test.ts
@@ -1,58 +1,107 @@
 import reducer, {
+  addBun,
   addIngredient,
   removeIngredient,
   moveIngredient,
-  initialState
+  clearConstructor
 } from '../src/services/constructor/slice';
-import { TConstructorIngredient } from '../src/utils/types';
+import { TConstructorIngredient, TIngredient } from '../src/utils/types';
 
-const ctorIng = (id: string, name: string): TConstructorIngredient => ({
-  _id: id,
-  name,
-  type: 'main',
-  proteins: 1,
-  fat: 1,
-  carbohydrates: 1,
-  calories: 1,
-  price: 1,
+const bun: TIngredient = {
+  _id: 'bun1',
+  name: 'Булка вкусная',
+  type: 'bun',
+  proteins: 10,
+  fat: 20,
+  carbohydrates: 30,
+  calories: 40,
+  price: 100,
+  image: 'img',
+  image_large: 'img_l',
+  image_mobile: 'img_m'
+};
+
+const ingredient1: TConstructorIngredient = {
+  _id: 'sauce1',
+  name: 'Соус классный',
+  type: 'sauce',
+  proteins: 5,
+  fat: 5,
+  carbohydrates: 5,
+  calories: 5,
+  price: 20,
   image: 'img',
   image_large: 'img_l',
   image_mobile: 'img_m',
-  id
-});
+  id: 'unique-id-1'
+};
+
+const ingredient2: TConstructorIngredient = {
+  _id: 'main1',
+  name: 'Мясо вкусное',
+  type: 'main',
+  proteins: 50,
+  fat: 50,
+  carbohydrates: 10,
+  calories: 300,
+  price: 200,
+  image: 'img',
+  image_large: 'img_l',
+  image_mobile: 'img_m',
+  id: 'unique-id-2'
+};
 
 describe('constructorSlice', () => {
-  it('добавляет ингредиент', () => {
-    const ingredient = ctorIng('1', 'some');
-    expect(reducer(initialState, addIngredient(ingredient))).toMatchObject({
-      ...initialState,
-      ingredients: [ingredient]
+  it('должен возвращать initialState', () => {
+    expect(reducer(undefined, { type: '' })).toEqual({
+      bun: null,
+      ingredients: []
     });
   });
 
-  it('удаляет ингредиент по id', () => {
-    const ing1 = ctorIng('id_1', 'item1');
-    const ing2 = ctorIng('id_2', 'item2');
-    const startState = {
-      ...initialState,
-      ingredients: [ing1, ing2]
-    };
-    expect(reducer(startState, removeIngredient('id_1')).ingredients).toEqual([
-      ing2
-    ]);
+  it('addBun работает', () => {
+    const state = reducer(undefined, addBun(bun));
+    expect(state.bun).toEqual(bun);
   });
 
-  it('перемещает ингредиенты', () => {
-    const A = ctorIng('A', 'A');
-    const B = ctorIng('B', 'B');
-    const C = ctorIng('C', 'C');
-    const startState = {
-      ...initialState,
-      ingredients: [A, B, C]
+  it('addIngredient работает', () => {
+    const state = reducer(undefined, addIngredient(ingredient1));
+    expect(state.ingredients).toHaveLength(1);
+    expect(state.ingredients[0]).toEqual(ingredient1);
+  });
+
+  it('removeIngredient работает', () => {
+    const stateWithIngredients = {
+      bun: null,
+      ingredients: [ingredient1, ingredient2]
     };
-    expect(
-      reducer(startState, moveIngredient({ fromIndex: 0, toIndex: 2 }))
-        .ingredients
-    ).toEqual([B, C, A]);
+    const state = reducer(
+      stateWithIngredients,
+      removeIngredient(ingredient1.id)
+    );
+    expect(state.ingredients).toHaveLength(1);
+    expect(state.ingredients[0]).toEqual(ingredient2);
+  });
+
+  it('moveIngredient работает', () => {
+    const stateWithIngredients = {
+      bun: null,
+      ingredients: [ingredient1, ingredient2]
+    };
+    const state = reducer(
+      stateWithIngredients,
+      moveIngredient({ fromIndex: 0, toIndex: 1 })
+    );
+    expect(state.ingredients[0]).toEqual(ingredient2);
+    expect(state.ingredients[1]).toEqual(ingredient1);
+  });
+
+  it('clearConstructor работает', () => {
+    const stateWithStuff = {
+      bun,
+      ingredients: [ingredient1, ingredient2]
+    };
+    const state = reducer(stateWithStuff, clearConstructor());
+    expect(state).toEqual({ bun: null, ingredients: [] });
   });
 });
