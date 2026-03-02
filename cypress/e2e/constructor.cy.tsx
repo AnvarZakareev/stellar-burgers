2976a144ed950a272fd7e7ded497280727d29979
AnvarZakareev <zanvarr@mail.ru>
2026-03-02 17:37:27 +0800

    test modal OK

--- cypress/e2e/constructor.cy.tsx
+++ cypress/e2e/constructor.cy.tsx
@@ -7,16 +7,24 @@ describe('Burger Constructor', () => {
     cy.wait('@getIngredients');
   });
 
-  it('Ддобавление ингредиента из списка в конструктор', () => {
-    cy.contains('Краторная булка N-200i').parent().contains('Добавить').click();
-    cy.contains('Мясо бессмертных моллюсков Protostomia')
-      .parent()
-      .contains('Добавить')
-      .click();
+  // it('Добавление ингредиента из списка в конструктор', () => {
+  //   cy.contains('Краторная булка N-200i').parent().contains('Добавить').click();
+  //   cy.contains('Мясо бессмертных моллюсков Protostomia')
+  //     .parent()
+  //     .contains('Добавить')
+  //     .click();
 
-    cy.get('[class*=constructor]').contains('Краторная булка N-200i');
-    cy.get('[class*=constructor]').contains(
-      'Мясо бессмертных моллюсков Protostomia'
-    );
+  //   cy.get('[class*=constructor]').contains('Краторная булка N-200i');
+  //   cy.get('[class*=constructor]').contains(
+  //     'Мясо бессмертных моллюсков Protostomia'
+  //   );
+  // });
+
+  it('Протестирована работа модальных окон', () => {
+    cy.contains('Краторная булка N-200i').click();
+    cy.contains('Детали ингредиента').should('be.visible');
+    cy.contains('Краторная булка N-200i').should('be.visible');
+    cy.get('button.Z7mUFPBZScxutAKTLKHN').click();
+    cy.contains('Детали ингредиента').should('not.exist');
   });
 });
